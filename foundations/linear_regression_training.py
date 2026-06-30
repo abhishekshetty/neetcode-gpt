bfcd9862da08ff52397f0877edae0174ca5e31da
Abhishek Shetty <abhishekshetty1993@gmail.com>
2026-06-30 20:42:41 +0530

    Update Linear regression training (Math Foundations 6/6)

--- foundations/linear_regression_training.py
+++ foundations/linear_regression_training.py
@@ -1,17 +1,28 @@
 import numpy as np
 from numpy.typing import NDArray
 
+
 class Solution:
+    def get_derivative(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64], N: int, X: NDArray[np.float64], desired_weight: int) -> float:
+        # note that N is just len(X)
+        return -2 * np.dot(ground_truth - model_prediction, X[:, desired_weight]) / N
 
     def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
-        # X is (n, m), weights is (m,) -> return (n,) predictions
-        # Round to 5 decimal places
-        pred = np.matmul(X ,weights)
-        return np.round(pred, 5)
-        
+        return np.squeeze(np.matmul(X, weights))
 
-    def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
-        # Compute mean squared error between predictions and ground truth
-        # Round to 5 decimal places
-        error =  np.mean(np.square(model_prediction - ground_truth))
-        return np.round(error, 5);
+    learning_rate = 0.01
+
+    def train_model(
+        self,
+        X: NDArray[np.float64],
+        Y: NDArray[np.float64],
+        num_iterations: int,
+        initial_weights: NDArray[np.float64]
+    ) -> NDArray[np.float64]:
+        for _ in range(num_iterations):
+            prediction = self.get_model_prediction(X, initial_weights)
+            for j in range(len(initial_weights)):
+                gradient = self.get_derivative(prediction, Y, len(X), X, j)
+                initial_weights[j] -= gradient * self.learning_rate
+
+        return np.round(initial_weights, 5)
